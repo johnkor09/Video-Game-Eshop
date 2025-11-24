cf9f5b108d0b17e7b3e19a0cae55b4b9affe2840
spaghettio245 <antoniotsalikicefala@gmail.com>
2025-11-24 16:47:18 +0200

    eimai xalia me ta animation

--- frontend/src/PageOfItem.js
+++ frontend/src/PageOfItem.js
@@ -6,24 +6,34 @@ function PageOfItem()
     return(
         <>
         <div className="PageOfItem">
-            
+            <div className="CentralPart">
             <div className="image">
-              <img src="C:\Users\antonio\OneDrive\Έγγραφα\GitHub\Video-Game-Eshop\frontend\src\IMGTODELETE\frierent.png" alt="placeholder einai cringe kai den fortonei h eikona deite to sto arxeio"></img>
-              </div>
-            <div className="Description">
-            <p>HERE IS DESCRIPTION</p> 
+             <img src="C:\Users\antonio\OneDrive\Έγγραφα\GitHub\Video-Game-Eshop\frontend\src\IMGTODELETE\frierent.png" alt="placeholder einai cringe kai den fortonei h eikona deite to sto arxeio"></img>
             </div>
-            <div className="idk"> sxolia? specs? consola? idk</div>
-            
+             
+              <div className="Description">
+                <p>HERE IS DESCRIPTION</p> 
+              </div>
+
+             <div className="idk"> sxolia? specs? consola? idk</div>
+            </div>
             
         
-        <div className="buttonPart">
-        <button className="back" onClick={()=>navigate("/")}>
-            back
-        </button>
-        <button className="add"> Add  </button>
-        <button className="wishlist">wishlist</button>
-        </div>
+             <div className="ButtonPart">
+              <div className="Buttons1">
+              <div><button className="back" onClick={()=>navigate("/")}> back</button></div>
+              <div><button className="wishlist">wishlist</button></div>
+              </div>
+              
+               <div className="Buttons2">
+               <div><button className="add"> Add  </button></div>
+                <div>
+                <label for="quantity"> quantity:</label>
+                <input type="number" name="quantity" min="1" max="99"></input>
+                </div>
+                </div>
+             </div>
+            
         </div>
         </>
     );
