43dbd02b31da233cde6c72f640f2143966a5d6a6
spaghettio245 <antoniotsalikicefala@gmail.com>
2025-12-05 16:50:15 +0200

    backend fixes

--- frontend/src/Items/PlaystationGames.js
+++ frontend/src/Items/PlaystationGames.js
@@ -11,7 +11,7 @@ export default function PlaystationGames() {
     useEffect(() => {
         const getGames = async () => {
             try {
-                const response = await axios.get('/api/games/playstation');
+                const response = await axios.get('http://localhost:5000/api/games/playstation');
                 setGames(response.data);
             } catch (err) {
                 console.error("Failed to get games data.", err);
