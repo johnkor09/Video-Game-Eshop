f314a8297f5f8b19d5906796601a440fab499838
johnkor09 <johnkor09@gmail.com>
2025-12-21 23:42:44 +0200

    Update HomePage.js

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,6 +1,5 @@
 import './HomePage.css';
-import{useEffect} from 'react';
-import {useState } from 'react';
+import { useState, useCallback,useEffect } from 'react';
 import IMG1 from './slide_images/pvz.jpg'
 import IMG2 from './slide_images/asuka.jpg'
 import IMG3 from './slide_images/oguri.jpeg'
@@ -10,51 +9,49 @@ import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
 
 export default function Home() {
-    const pictures=[IMG1,IMG2,IMG3,IMG4,IMG5];
-    const [picIndex,setIndex]=useState(0);
+  const pictures = [IMG1, IMG2, IMG3, IMG4, IMG5];
+  const [picIndex, setIndex] = useState(0);
 
-  useEffect(()=>{
-    const inter=setInterval(goNext,5000);
-    return() => clearInterval(inter);
-  },[]);
-  
-    function goNext()
-    {
-      setIndex(index=>{
-        if(index==pictures.length - 1) 
-          return 0;
-        else
-          return index + 1;
-      })
-    }
-    
-    function goPrev()
-    {
-      setIndex(index=>{
-        if(index==0) 
-          return pictures.length -1;
-        else
-          return index-1;
-      })
-    }
-  
-    return(
-        <>
-        <div className='slider'>
-        
-         <div className='animationPart'>
-              {pictures.map(pic => (
-            <img key={pic} src={pic} className='image' style={{translate:`${-100 * picIndex}%` }}/> ))}
-         </div>
+  const goNext = useCallback(() => {
+    setIndex(index => {
+      if (index === pictures.length - 1)
+        return 0;
+      else
+        return index + 1;
+    })
+  }, [pictures.length]);
 
-          <button className='leftArrow' onClick={goPrev} ><FaArrowLeft className='arrowL' /></button>
-          <button className='rightArrow' onClick={goNext}><FaArrowRight className='arrowR'/></button>
-          
-         
+  useEffect(() => {
+    const inter = setInterval(goNext, 5000);
+    return () => clearInterval(inter);
+  }, [goNext]);
+
+  const goPrev = () => {
+    setIndex(index => {
+      if (index === 0)
+        return pictures.length - 1;
+      else
+        return index - 1;
+    })
+  };
+
+  return (
+    <>
+      <div className='slider'>
+
+        <div className='animationPart'>
+          {pictures.map(pic => (
+            <img key={pic} src={pic} alt='' className='image' style={{ translate: `${-100 * picIndex}%` }} />))}
         </div>
-            
-        </>
-    )
+
+        <button className='leftArrow' onClick={goPrev} ><FaArrowLeft className='arrowL' /></button>
+        <button className='rightArrow' onClick={goNext}><FaArrowRight className='arrowR' /></button>
+
+
+      </div>
+
+    </>
+  )
 }
 
 
